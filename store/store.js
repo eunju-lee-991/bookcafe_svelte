9e12b5272519d4025fc244ba2750d15a05f34c9d
eunju <eunju7757@gmail.com>
2023-03-15 22:05:52 +0900

    remove store file

--- store/store.js
+++
@@ -1,4 +0,0 @@
-import { writable, readable } from 'svelte/store';
-
-export const REST_API_KEY = readable('7bcfc7029ccb017c031af94a6e2dd46a');
-export const REDIRECT_URI = readable('http://localhost:5000/login/token');
